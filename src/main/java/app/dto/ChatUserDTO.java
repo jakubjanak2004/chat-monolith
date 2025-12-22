a42e9d2a39f7d558c560af4784c07e87e7a8ffd1
Jakub Janak <janakja5@fel.cvut.cz>
2025-12-22 14:12:44 +0100

    created chat user dto that maps the user out of the system

---
+++ src/main/java/app/dto/ChatUserDTO.java
@@ -0,0 +1,12 @@
+package app.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ChatUserDTO {
+    private String username;
+    private String firstName;
+    private String lastName;
+}
